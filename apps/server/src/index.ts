c3f4da4e0f88baa51a928085bda6880ce1a9d0d5
lightyaer <mmnyed@gmail.com>
2025-08-07 07:04:47 +0530

    try deploy 2

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -13,11 +13,11 @@ app.use(logger());
 app.use(
   "/*",
   cors({
-    origin: process.env.CORS_ORIGIN || "",
-    allowMethods: ["GET", "POST", "OPTIONS"],
+    origin: "*",
+    allowMethods: ["GET", "POST", "OPTIONS", "HEAD", "PUT", "DELETE"],
     allowHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  }),
+  })
 );
 
 app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
